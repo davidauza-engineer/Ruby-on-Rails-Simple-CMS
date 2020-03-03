e7f982444e95af90d4dc7364d1ede30ef5e1773b
davidauza.engineer <davidauza01@gmail.com>
2020-03-03 08:19:04 -0500

    Explore routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  get 'test/index'
+  get '/testing', to: 'test#index'
+  get 'test/show'
+  get 'test/new'
+  get 'test/edit'
+  root 'demo#index'
+
+  get 'demo/index'
+  get 'demo/hello'
+
+  # default route
+  # may go away in future versions of Rails
+  # get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
