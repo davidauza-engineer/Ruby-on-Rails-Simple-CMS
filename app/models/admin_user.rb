b717942ab90925d78dc4ea49eca66d30970f2567
davidauza.engineer <davidauza01@gmail.com>
2020-04-05 10:21:42 -0500

    Add scopes to Subject

---
+++ app/models/admin_user.rb
@@ -0,0 +1,4 @@
+class AdminsuUser < ApplicationRecord
+
+  # self.table_name = 'admin_users'
+end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,9 @@
 class Subject < ApplicationRecord
+
+  # A scope is basically naming a specific query.
+  scope :visible, lambda { where(visible: true) }
+  scope :invisible, lambda { where(visible: false) }
+  scope :sorted, -> { order(position: :asc) }
+  scope :newest_first, lambda { order(created_at: :desc) }
+  scope :search, lambda { |query| where(['name LIKE ?', "%#{query}%"]) }
 end
